4266ab05dbfeb558d2596c68fe3792bf970f0161
lucktroy <jbzhang86@gmail.com>
2014-08-03 23:41:19 +0800

    deploy blog

--- _site/cv/runMe.sh
+++ _site/cv/runMe.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-#pandoc --from markdown -o zhang-cv.tex zhang-cv.md --template=pm-template.latex
-pandoc --from markdown --latex-engine=xelatex -o zhang-cv.pdf zhang-cv.md --template=pm-template.latex
-#pandoc --standalone -c style.css --from markdown --to html -o zhang-cv.html zhang-cv.md
+
+#pandoc --from markdown --latex-engine=xelatex -o zhang-cv.pdf zhang-cv.md --template=pm-template.latex
+
+pandoc --from markdown --latex-engine=xelatex -o zhang-cv.tex zhang-cv.md --template=pm-template.latex
+
+latexmk -pdf zhang-cv.tex
+
 open zhang-cv.pdf
