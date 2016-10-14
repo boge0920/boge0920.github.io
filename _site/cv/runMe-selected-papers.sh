9cc1b263f868333374921d8638ccba37edd62d43
lucktroy <jbzhang86@gmail.com>
2016-10-15 01:34:09 +0800

    deploy blog

--- _site/cv/runMe-selected-papers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#pandoc --from markdown --latex-engine=xelatex -o zhang-cv.pdf zhang-cv.md --template=pm-template.latex
-
-pandoc --from markdown --latex-engine=xelatex -o zhang-cv-selected-papers.tex zhang-cv-selected-papers.md --template=pm-template.latex
-
-latexmk --xelatex -pdf zhang-cv-selected-papers.tex
-
-open zhang-cv-selected-papers.pdf
