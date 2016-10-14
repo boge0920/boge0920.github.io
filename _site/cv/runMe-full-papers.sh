1ae6209fd2433a44b6e44921a04fcb8df43f9386
lucktroy <jbzhang86@gmail.com>
2016-10-15 02:18:16 +0800

    deploy blog

--- _site/cv/runMe-full-papers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#pandoc --from markdown --latex-engine=xelatex -o zhang-cv.pdf zhang-cv.md --template=pm-template.latex
-
-pandoc --from markdown --latex-engine=xelatex -o zhang-cv-full-papers.tex zhang-cv-full-papers.md --template=pm-template.latex
-
-latexmk -pdf zhang-cv-full-papers.tex
-
-open zhang-cv-full-papers.pdf
