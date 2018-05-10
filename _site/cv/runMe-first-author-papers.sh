edd6e99e11fc208e29f8d7a3f9f707f2a4debfbf
lucktroy <jbzhang86@gmail.com>
2018-05-10 10:40:50 +0800

    deploy blog

--- _site/cv/runMe-first-author-papers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#pandoc --from markdown --latex-engine=xelatex -o zhang-cv.pdf zhang-cv.md --template=pm-template.latex
-
-pandoc --from markdown --latex-engine=xelatex -o zhang-cv-first-author-papers.tex zhang-cv-first-author-papers.md --template=pm-template.latex
-
-latexmk -pdf zhang-cv-first-author-papers.tex
-
-open zhang-cv-first-author-papers.pdf
